aa59639d9b8d2e711ad3f493059ff88935b9d60d
andrewbowen19 <atbowen@gmail.com>
2022-10-22 13:26:33 -0400

    add bls summary

--- R/bls.R
+++ R/bls.R
@@ -10,16 +10,20 @@ df <- read.csv(github_url)
 
 head(df)
 
-#df %>% filter(OCC_TITLE=="Data Scientists")
-df$A_MEAN <- as.double(df$A_MEAN)
-df$A_MEDIAN <- as.double(df$A_MEDIAN)
+# casting to numeric types for aggregation
+df$A_MEAN <- as.numeric(df$A_MEAN)
+df$A_MEDIAN <- as.numeric(df$A_MEDIAN)
+
 
 ds_data <- df %>% filter(OCC_TITLE=="Data Scientists")
 ds_mean_salary <- ds_data$A_MEAN[[1]][1]
 ds_median_salary <- ds_data$A_MEDIAN[[1]][1]
 
 # Quick summary 
-summarise(annueal_mean_salary = mean(A_MEAN), median = median(A_MEAN))
+df %>% summarise(annual_mean_salary = mean(A_MEAN, na.rm = TRUE),
+                 annual_median_salary = median(A_MEDIAN, na.rm = TRUE),
+                 ds_annual_mean = ds_mean_salary,
+                 ds_annual_median = ds_median_salary)
 
 # Taking a quick look at the distributon of mean and median annual salaries across all occupations
 a_mean_dist <- ggplot(df, aes(x=A_MEAN)) + geom_histogram(bins=25) + geom_vline(xintercept=ds_mean_salary)  + ggtitle("Annual Mean Salary: all occupations")
@@ -34,5 +38,11 @@ a_mean_box
 
 a_median_box <- ggplot(df, aes(x=A_MEDIAN)) + geom_boxplot() + ggtitle("Annual Median Salary: all occupations")
 a_median_box
+
+
+
+
+
+
 
 
