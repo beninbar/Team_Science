30a2d890316c329b0045a58464fe89410d82f439
andrewbowen19 <atbowen@gmail.com>
2022-10-22 13:17:56 -0400

    add bls plot titles

--- R/bls.R
+++ R/bls.R
@@ -2,7 +2,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-# Readin converted csv file
+# Reading converted csv file
+# original data link: https://www.bls.gov/oes/current/oes_nat.htm
 github_url <- "https://raw.githubusercontent.com/Anthogonyst/Team_Science/master/data/bls_occupational_wage_data.csv"
 
 df <- read.csv(github_url)
@@ -17,16 +18,21 @@ ds_data <- df %>% filter(OCC_TITLE=="Data Scientists")
 ds_mean_salary <- ds_data$A_MEAN[[1]][1]
 ds_median_salary <- ds_data$A_MEDIAN[[1]][1]
 
+# Quick summary 
+summarise(annueal_mean_salary = mean(A_MEAN), median = median(A_MEAN))
+
 # Taking a quick look at the distributon of mean and median annual salaries across all occupations
-a_mean_dist <- ggplot(df, aes(x=A_MEAN)) + geom_histogram() + geom_vline(xintercept=ds_mean_salary) #+ annotate("text", x = 1.5 * ds_mean_salary, y = 300, label="Average DS salary")
+a_mean_dist <- ggplot(df, aes(x=A_MEAN)) + geom_histogram(bins=25) + geom_vline(xintercept=ds_mean_salary)  + ggtitle("Annual Mean Salary: all occupations")
 a_mean_dist
 
-a_median_dist <- ggplot(df, aes(x=A_MEDIAN)) + geom_histogram() + geom_vline(aes(xintercept=ds_median_salary))
+a_median_dist <- ggplot(df, aes(x=A_MEDIAN)) + 
+  geom_histogram(bins=25) + geom_vline(aes(xintercept=ds_median_salary)) + ggtitle("Annual Median Salary: all occupations")
 a_median_dist
 
-a_mean_box <- ggplot(df, aes(x=A_MEAN)) + geom_boxplot()
+a_mean_box <- ggplot(df, aes(x=A_MEAN)) + geom_boxplot() + ggtitle("Annual Mean Salary: all occupations")
 a_mean_box
 
-a_median_box <- ggplot(df, aes(x=A_MEDIAN)) + geom_boxplot()
+a_median_box <- ggplot(df, aes(x=A_MEDIAN)) + geom_boxplot() + ggtitle("Annual Median Salary: all occupations")
 a_median_box
-  
+
+
