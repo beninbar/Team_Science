498b3a9c79ab215f2ce3f37e59976b73410f62ba
andrewbowen19 <atbowen@gmail.com>
2022-10-22 12:14:02 -0400

    add json normalize to comp call

--- Python/sec_filings.py
+++ Python/sec_filings.py
@@ -6,6 +6,13 @@ Will pull data for a few larger Fortune 500 companies.
 Ideally we'd want to 
 """
 import requests
+import os
+import json
+import pandas as pd
+
+# Free API keys can be downloaded here
+# https://sec-api.io/signup/free
+SEC_API_KEY = os.environ.get("SEC_API_KEY")
 
 def get_submission(cik):
     """
@@ -14,10 +21,20 @@ def get_submission(cik):
     parameters:
         cik: str or int; Central Index Key/Company Identifier outlined by SEC Filings API
     """
-    url = f"https://data.sec.gov/submissions/{cik}.json"
+    url = f"data.sec.gov/api/xbrl/companyfacts/{cik}.json" #f"https://data.sec.gov/submissions/{cik}.json"
     r = requests.get(url)
-    print(r.text)
+
     return r.text
+
+def get_compensation(cik):
+    """Pulls executive compensation data for a given company cia the SEC API
+    
+    parameters:
+        cik: int or str; Central Index Key to identify company being queried
+    """
+    url = f"https://api.sec-api.io/compensation/{cik}?token={SEC_API_KEY}"
+
+    return requests.get(url)
 
 if __name__ == "__main__":
     company_ciks = {
@@ -26,6 +43,10 @@ if __name__ == "__main__":
                    }
     for company, cik  in company_ciks.items():
 
-        txt = get_submission(cik)
+        comp = get_compensation(cik)
+        comp_json = comp.json()
+
+        df = pd.json_normalize(comp_json)
+        print(pd.json_normalize(comp_json))
 
         print("-------------------")
